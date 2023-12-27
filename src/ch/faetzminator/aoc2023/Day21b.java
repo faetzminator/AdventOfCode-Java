3ef041deb43a474e7124a9f51b50a80cdfe88ea2
faetzminator <faetzminator@gmail.com>
2023-12-27 23:07:37 +0100

    day 21b the proper math way

--- src/ch/faetzminator/aoc2023/Day21b.java
+++ src/ch/faetzminator/aoc2023/Day21b.java
@@ -13,10 +13,10 @@ import ch.faetzminator.aocutil.PuzzleUtil;
 import ch.faetzminator.aocutil.ScannerUtil;
 import ch.faetzminator.aocutil.Timer;
 
-public class Day21 {
+public class Day21b {
 
     public static void main(final String[] args) {
-        final Day21 puzzle = new Day21();
+        final Day21b puzzle = new Day21b();
 
         final List<String> lines = ScannerUtil.readNonBlankLines();
         final Timer timer = PuzzleUtil.start();
@@ -40,34 +40,89 @@ public class Day21 {
         }
     }
 
-    private static final int MAX_LENGTH = 64;
+    private static final int MAX_LENGTH = 26501365;
 
     public long countReachableGardenPlots() {
-        long sum = 1L;
+
+        // any EVEN field (e.g. the middle containing start position) needs the odd sum
+        // since MAX_LENGTH is odd - vice versa for any ODD field (e.g. 0/1, -1/0 etc)
+        final int EVEN = 1;
+        final int ODD = 0;
+
+        final int length = map.getXSize();
+        if (length != map.getYSize()) {
+            throw new IllegalArgumentException("needs same x and y length");
+        }
+        final long multiplier = MAX_LENGTH / length;
+        if (MAX_LENGTH % length - length / 2 != 0) {
+            throw new IllegalArgumentException("remainer 0 needed");
+        }
+
+        final long[] entireField = countReachableGardenPlots(map.getStartPosition(), Integer.MAX_VALUE);
+
+        long result = 0L;
+        // add entirely reachable fields
+        result += (multiplier - 1L) * (multiplier - 1L) * entireField[EVEN];
+        result += multiplier * multiplier * entireField[ODD];
+
+        final int left = 0, middle = length / 2, right = length - 1;
+        final int distance = length;
+        // add corners
+        // note: each corner is EVEN but it's ODD to start point (middle = +65)
+        result += countReachableGardenPlots(new Position(middle, left), distance)[ODD];
+        result += countReachableGardenPlots(new Position(middle, right), distance)[ODD];
+        result += countReachableGardenPlots(new Position(left, middle), distance)[ODD];
+        result += countReachableGardenPlots(new Position(right, middle), distance)[ODD];
+
+        final int longDistance = length + length / 2;
+        final int shortDistance = length / 2;
+        // add other outer fields
+        for (final int x : new int[] { left, right }) {
+            for (final int y : new int[] { left, right }) {
+                final Position position = new Position(x, y);
+                result += (multiplier - 1L) * countReachableGardenPlots(position, longDistance)[EVEN];
+                result += multiplier * countReachableGardenPlots(position, shortDistance)[ODD];
+            }
+        }
+
+        return result;
+    }
+
+    private long[] countReachableGardenPlots(final Position startPosition, final int limit) {
+
+        final long[] sums = new long[2];
         final Queue<BlockAtPosition> queue = new LinkedList<>();
-        final BlockAtPosition startBlock = map.getElementAt(map.getStartPosition());
+        final BlockAtPosition startBlock = map.getElementAt(startPosition);
         startBlock.setDistance(0);
         queue.add(startBlock);
+        sums[0]++;
 
         while (!queue.isEmpty()) {
             final BlockAtPosition current = queue.poll();
             final int nextDistance = current.getDistance() + 1;
-            if (nextDistance <= MAX_LENGTH) {
+            if (nextDistance < limit) {
                 for (final Direction direction : Direction.values()) {
                     final Position nextPos = current.getPosition().move(direction);
                     final BlockAtPosition nextBlock = map.getElementAt(nextPos);
                     if (nextBlock != null && nextBlock.getElement() != Block.ROCK && !nextBlock.hasDistance()) {
                         nextBlock.setDistance(nextDistance);
                         queue.add(nextBlock);
-                        if (nextDistance % 2 == 0) {
-                            sum++;
-                        }
+                        sums[nextDistance % 2]++;
                     }
                 }
             }
         }
 
-        return sum;
+        reset();
+        return sums;
+    }
+
+    private void reset() {
+        for (int x = 0; x < map.getXSize(); x++) {
+            for (int y = 0; y < map.getYSize(); y++) {
+                map.getElementAt(x, y).reset();
+            }
+        }
     }
 
     private static class BlockAtPosition extends ElementAtPosition<Block> {
@@ -92,10 +147,14 @@ public class Day21 {
             return distance != UNSET;
         }
 
+        public void reset() {
+            distance = UNSET;
+        }
+
         @Override
         public char toPrintableChar() {
-            if (getElement() == Block.GARDEN_PLOT && distance % 2 == 0) {
-                return (char) (distance % 10 + '0');
+            if (getElement() == Block.GARDEN_PLOT && hasDistance()) {
+                return distance % 2 == 0 ? '*' : '+';
             }
             return super.toPrintableChar();
         }
