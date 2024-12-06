3b9af9afff990fb69f4e90503f2810dbc82ccfba
faetzminator <faetzminator@gmail.com>
2024-12-06 14:33:47 +0100

    day 6

---
+++ aoc2024/src/main/java/ch/faetzminator/aoc2024/Day06.java
@@ -0,0 +1,102 @@
+package ch.faetzminator.aoc2024;
+
+import java.util.List;
+
+import ch.faetzminator.aocutil.CharEnum;
+import ch.faetzminator.aocutil.Direction;
+import ch.faetzminator.aocutil.PuzzleUtil;
+import ch.faetzminator.aocutil.ScannerUtil;
+import ch.faetzminator.aocutil.Timer;
+import ch.faetzminator.aocutil.map.ElementAtPosition;
+import ch.faetzminator.aocutil.map.PMapFactory;
+import ch.faetzminator.aocutil.map.PMapWithStart;
+import ch.faetzminator.aocutil.map.Position;
+
+public class Day06 {
+
+    public static void main(final String[] args) {
+        final Day06 puzzle = new Day06();
+        final List<String> lines = ScannerUtil.readNonBlankLines();
+        final Timer timer = PuzzleUtil.start();
+        puzzle.parseLines(lines);
+        final long solution = puzzle.walk();
+        PuzzleUtil.end(solution, timer);
+    }
+
+    private PMapWithStart<BlockAtPosition> map;
+
+    public void parseLines(final List<String> input) {
+        map = new PMapFactory<>(BlockAtPosition.class,
+                (position, character) -> new BlockAtPosition(position, Block.byChar(character)))
+                .create(input, element -> element.getElement() == Block.START);
+    }
+
+    public long walk() {
+        Direction direction = Direction.NORTH;
+        BlockAtPosition block = map.getStartElement();
+        block.setVisited();
+
+        while (true) {
+            BlockAtPosition nextBlock;
+            Position nextPos;
+            do {
+                nextPos = block.getPosition().move(direction);
+                nextBlock = map.getElementAt(nextPos);
+                if (nextBlock == null) {
+                    // we're outside of the map, so let's count
+                    return map.stream().filter(BlockAtPosition::isVisited).count();
+                }
+                if (nextBlock.getElement() == Block.OBSTRUCTION) {
+                    direction = direction.getClockwise();
+                }
+            } while (nextBlock.getElement() == Block.OBSTRUCTION);
+            nextBlock.setVisited();
+            block = nextBlock;
+        }
+    }
+
+    private static class BlockAtPosition extends ElementAtPosition<Block> {
+
+        private boolean visited;
+
+        public BlockAtPosition(final Position position, final Block block) {
+            super(block, position);
+        }
+
+        public void setVisited() {
+            visited = true;
+        }
+
+        public boolean isVisited() {
+            return visited;
+        }
+
+        @Override
+        public char toPrintableChar() {
+            if (visited) {
+                return 'X';
+            }
+            return super.toPrintableChar();
+        }
+    }
+
+    private static enum Block implements CharEnum {
+
+        PATH('.'), OBSTRUCTION('#'), START('^');
+
+        private final char character;
+
+        private Block(final char character) {
+            this.character = character;
+        }
+
+        @Override
+        public char getCharacter() {
+            return character;
+        }
+
+        public static Block byChar(final char c) {
+            return CharEnum.byChar(Block.class, c);
+        }
+    }
+}
---
+++ aoc2024/src/main/java/ch/faetzminator/aoc2024/Day06b.java
@@ -0,0 +1,128 @@
+package ch.faetzminator.aoc2024;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import ch.faetzminator.aocutil.CharEnum;
+import ch.faetzminator.aocutil.Direction;
+import ch.faetzminator.aocutil.PuzzleUtil;
+import ch.faetzminator.aocutil.ScannerUtil;
+import ch.faetzminator.aocutil.Timer;
+import ch.faetzminator.aocutil.map.ElementAtPosition;
+import ch.faetzminator.aocutil.map.PMapFactory;
+import ch.faetzminator.aocutil.map.PMapWithStart;
+import ch.faetzminator.aocutil.map.Position;
+
+public class Day06b {
+
+    public static void main(final String[] args) {
+        final Day06b puzzle = new Day06b();
+        final List<String> lines = ScannerUtil.readNonBlankLines();
+        final Timer timer = PuzzleUtil.start();
+        puzzle.parseLines(lines);
+        final long solution = puzzle.walkAllPossibilities();
+        PuzzleUtil.end(solution, timer);
+    }
+
+    private PMapWithStart<BlockAtPosition> map;
+
+    public void parseLines(final List<String> input) {
+        map = new PMapFactory<>(BlockAtPosition.class,
+                (position, character) -> new BlockAtPosition(position, Block.byChar(character)))
+                .create(input, element -> element.getElement() == Block.START);
+    }
+
+    public long walkAllPossibilities() {
+        return map.stream().filter(element -> {
+            boolean keep = false;
+            if (element.getElement() == Block.PATH) {
+                map.setElementAt(element.getPosition(), new BlockAtPosition(element.getPosition(), Block.OBSTRUCTION));
+                keep = walk();
+                map.setElementAt(element.getPosition(), element);
+                map.stream().forEach(BlockAtPosition::reset);
+            }
+            return keep;
+        }).count();
+    }
+
+    private boolean walk() {
+        Direction direction = Direction.NORTH;
+        BlockAtPosition block = map.getStartElement();
+        block.setVisited(direction);
+
+        while (true) {
+            BlockAtPosition nextBlock;
+            Position nextPos;
+            do {
+                nextPos = block.getPosition().move(direction);
+                nextBlock = map.getElementAt(nextPos);
+                if (nextBlock == null) {
+                    // we're outside of the map - no loop
+                    return false;
+                }
+                if (nextBlock.isVisited(direction)) {
+                    // we're stuck in a loop
+                    return true;
+                }
+                if (nextBlock.getElement() == Block.OBSTRUCTION) {
+                    direction = direction.getClockwise();
+                }
+            } while (nextBlock.getElement() == Block.OBSTRUCTION);
+            if (nextBlock.isVisited(direction)) {
+                return true;
+            }
+            nextBlock.setVisited(direction);
+            block = nextBlock;
+        }
+    }
+
+    private static class BlockAtPosition extends ElementAtPosition<Block> {
+
+        private Set<Direction> visitedDirections = new HashSet<>();
+
+        public BlockAtPosition(final Position position, final Block block) {
+            super(block, position);
+        }
+
+        public void setVisited(final Direction direction) {
+            visitedDirections.add(direction);
+        }
+
+        public boolean isVisited(final Direction direction) {
+            return visitedDirections.contains(direction);
+        }
+
+        public void reset() {
+            visitedDirections = new HashSet<>();
+        }
+
+        @Override
+        public char toPrintableChar() {
+            if (!visitedDirections.isEmpty()) {
+                return 'X';
+            }
+            return super.toPrintableChar();
+        }
+    }
+
+    private static enum Block implements CharEnum {
+
+        PATH('.'), OBSTRUCTION('#'), START('^');
+
+        private final char character;
+
+        private Block(final char character) {
+            this.character = character;
+        }
+
+        @Override
+        public char getCharacter() {
+            return character;
+        }
+
+        public static Block byChar(final char c) {
+            return CharEnum.byChar(Block.class, c);
+        }
+    }
+}
