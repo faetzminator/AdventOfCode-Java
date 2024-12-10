7a5398c974e9de8529def4ccc544bf782b7945c5
faetzminator <faetzminator@gmail.com>
2024-12-10 06:54:11 +0100

    day 10

---
+++ aoc2024/src/main/java/ch/faetzminator/aoc2024/Day10b.java
@@ -0,0 +1,103 @@
+package ch.faetzminator.aoc2024;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+import ch.faetzminator.aocutil.CharPrintable;
+import ch.faetzminator.aocutil.Direction;
+import ch.faetzminator.aocutil.PuzzleUtil;
+import ch.faetzminator.aocutil.ScannerUtil;
+import ch.faetzminator.aocutil.Timer;
+import ch.faetzminator.aocutil.map.ElementAtPosition;
+import ch.faetzminator.aocutil.map.PMap;
+import ch.faetzminator.aocutil.map.PMapFactory;
+import ch.faetzminator.aocutil.map.Position;
+
+public class Day10b {
+
+    public static void main(final String[] args) {
+        final Day10b puzzle = new Day10b();
+        final List<String> lines = ScannerUtil.readNonBlankLines();
+        final Timer timer = PuzzleUtil.start();
+        puzzle.parseLines(lines);
+        final long solution = puzzle.getTrailheadSum();
+        PuzzleUtil.end(solution, timer);
+    }
+
+    private PMap<HeightAtPosition> map;
+
+    public void parseLines(final List<String> input) {
+        map = new PMapFactory<>(HeightAtPosition.class,
+                (character, position) -> new HeightAtPosition(new Height(character), position)).create(input);
+    }
+
+    public long getTrailheadSum() {
+        long sum = 0L;
+        for (final HeightAtPosition element : map.stream().filter(HeightAtPosition::isStartPoint)
+                .collect(Collectors.toList())) {
+            sum += getTrailheadSum(element);
+        }
+        return sum;
+    }
+
+    private long getTrailheadSum(final HeightAtPosition start) {
+        final Queue<HeightAtPosition> queue = new LinkedList<>();
+        queue.add(start);
+        long sum = 0L;
+
+        while (!queue.isEmpty()) {
+            final HeightAtPosition current = queue.poll();
+            for (final Direction direction : Direction.values()) {
+                final Position nextPosition = current.getPosition().move(direction);
+                final HeightAtPosition next = map.getElementAt(nextPosition);
+                if (current.isNextStep(next)) {
+                    if (next.isEndPoint()) {
+                        sum++;
+                    } else {
+                        queue.add(next);
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static class HeightAtPosition extends ElementAtPosition<Height> {
+
+        public HeightAtPosition(final Height height, final Position position) {
+            super(height, position);
+        }
+
+        public boolean isNextStep(final HeightAtPosition other) {
+            return other != null && other.getElement().getHeight() == getElement().getHeight() + 1;
+        }
+
+        public boolean isStartPoint() {
+            return getElement().getHeight() == '0';
+        }
+
+        public boolean isEndPoint() {
+            return getElement().getHeight() == '9';
+        }
+    }
+
+    private static class Height implements CharPrintable {
+
+        private final char height;
+
+        public Height(final char height) {
+            this.height = height;
+        }
+
+        public char getHeight() {
+            return height;
+        }
+
+        @Override
+        public char toPrintableChar() {
+            return height;
+        }
+    }
+}
