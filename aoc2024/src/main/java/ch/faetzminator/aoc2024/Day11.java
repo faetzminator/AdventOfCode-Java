35a7ea423c4d6ad0608c4d440c53cee5cd407adb
faetzminator <faetzminator@gmail.com>
2024-12-11 07:53:10 +0100

    day 11 part 1

---
+++ aoc2024/src/main/java/ch/faetzminator/aoc2024/Day11.java
@@ -0,0 +1,54 @@
+package ch.faetzminator.aoc2024;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.faetzminator.aocutil.MathUtil;
+import ch.faetzminator.aocutil.PuzzleUtil;
+import ch.faetzminator.aocutil.ScannerUtil;
+import ch.faetzminator.aocutil.Timer;
+
+public class Day11 {
+
+    public static void main(final String[] args) {
+        final Day11 puzzle = new Day11();
+        final String line = ScannerUtil.readNonBlankLine();
+        final Timer timer = PuzzleUtil.start();
+        puzzle.parseLine(line);
+        final long solution = puzzle.blinkMultipleTimes();
+        PuzzleUtil.end(solution, timer);
+    }
+
+    private List<Long> stones;
+
+    public void parseLine(final String input) {
+        stones = new ArrayList<>();
+        for (final String s : input.split(" ")) {
+            stones.add(Long.valueOf(s));
+        }
+    }
+
+    private long blinkMultipleTimes() {
+        for (int i = 0; i < 25; i++) {
+            blink();
+        }
+        return stones.size();
+    }
+
+    private void blink() {
+        final List<Long> newStones = new ArrayList<>();
+        for (final long stone : stones) {
+            final int digits = MathUtil.countDigits(stone);
+            if (stone == 0L) {
+                newStones.add(1L);
+            } else if (digits % 2 == 0) {
+                final long divisor = MathUtil.pow10(digits / 2);
+                newStones.add(stone / divisor);
+                newStones.add(stone % divisor);
+            } else {
+                newStones.add(stone * 2024);
+            }
+        }
+        stones = newStones;
+    }
+}
