ca6f77b8f2167949f0807571ed8ae3f12bc66044
faetzminator <faetzminator@gmail.com>
2024-12-15 20:20:21 +0100

    day 15 part 2

--- aoc2024/src/main/java/ch/faetzminator/aoc2024/Day15b.java
+++ aoc2024/src/main/java/ch/faetzminator/aoc2024/Day15b.java
@@ -1,8 +1,13 @@
 package ch.faetzminator.aoc2024;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 import ch.faetzminator.aocutil.CharEnum;
 import ch.faetzminator.aocutil.Direction;
@@ -14,10 +19,10 @@ import ch.faetzminator.aocutil.map.PMapFactory;
 import ch.faetzminator.aocutil.map.PMapWithStart;
 import ch.faetzminator.aocutil.map.Position;
 
-public class Day15 {
+public class Day15b {
 
     public static void main(final String[] args) {
-        final Day15 puzzle = new Day15();
+        final Day15b puzzle = new Day15b();
         final List<String> mapLines;
         final List<String> moveLines;
         try (Scanner scanner = new Scanner(System.in)) {
@@ -36,7 +41,11 @@ public class Day15 {
     private final List<Move> moves = new ArrayList<>();
 
     public void parseMap(final List<String> lines) {
-        map = new PMapFactory<>(Block.class, (character, position) -> Block.byChar(character)).create(lines,
+        final List<String> expandedLines = new ArrayList<>();
+        for (final String line : lines) {
+            expandedLines.add(line.replace("#", "##").replace("O", "[]").replace(".", "..").replace("@", "@."));
+        }
+        map = new PMapFactory<>(Block.class, (character, position) -> Block.byChar(character)).create(expandedLines,
                 block -> block == Block.ROBOT);
     }
 
@@ -61,7 +70,9 @@ public class Day15 {
     private boolean tryMove(final Position startPosition, final Direction direction) {
         final MoveData data = getMovableBlocks(startPosition, direction);
         if (data.canMove()) {
-            map.setElementAt(startPosition, Block.PATH);
+            for (final Position position : data.getPositionsToUnset()) {
+                map.setElementAt(position, Block.PATH);
+            }
             for (final BlockAtPosition blockAtPosition : data.getBlocksToMove()) {
                 map.setElementAt(blockAtPosition.getPosition().move(direction), blockAtPosition.getElement());
             }
@@ -71,26 +82,59 @@ public class Day15 {
 
     private MoveData getMovableBlocks(final Position startPosition, final Direction direction) {
         final List<BlockAtPosition> blocksToMove = new ArrayList<>();
+        final List<Position> positionsToUnset = new ArrayList<>();
+        positionsToUnset.add(startPosition);
 
-        Position position = startPosition;
-        Block block = map.getElementAt(startPosition);
+        final Direction oppositeDirection = direction.getOpposite();
+
+        Collection<BlockAtPosition> lastBlocks = Arrays
+                .asList(new BlockAtPosition(map.getElementAt(startPosition), startPosition));
+
+        final Set<Block> seenBlocks = new HashSet<>();
         do {
-            blocksToMove.add(new BlockAtPosition(block, position));
-            position = position.move(direction);
-            block = map.getElementAt(position);
-            if (block == Block.ROCK) {
-                return new MoveData();
-            }
-        } while (block != Block.PATH);
+            seenBlocks.clear();
 
-        return new MoveData(blocksToMove);
+            final Collection<BlockAtPosition> blocks = new LinkedHashSet<>();
+            final Collection<BlockAtPosition> newBlocks = new LinkedHashSet<>();
+            for (final BlockAtPosition lastBlock : lastBlocks) {
+                blocksToMove.add(lastBlock);
+                final Position position = lastBlock.getPosition().move(direction);
+                final Block block = map.getElementAt(position);
+                if (block == Block.ROCK) {
+                    return new MoveData();
+                }
+                if (block == Block.PATH) {
+                    continue;
+                }
+                blocks.add(new BlockAtPosition(block, position));
+                seenBlocks.add(block);
+
+                final Direction neighbourDirection = block.getNeighbourDirection();
+                if (neighbourDirection != null && neighbourDirection != direction
+                        && neighbourDirection != oppositeDirection) {
+
+                    final Position neighbourPosition = position.move(neighbourDirection);
+                    newBlocks.add(new BlockAtPosition(map.getElementAt(neighbourPosition), neighbourPosition));
+                }
+            }
+            newBlocks.removeAll(blocks);
+            for (final BlockAtPosition newBlock : newBlocks) {
+                positionsToUnset.add(newBlock.getPosition());
+            }
+            blocks.addAll(newBlocks);
+
+            lastBlocks = blocks;
+            seenBlocks.remove(Block.PATH);
+        } while (!seenBlocks.isEmpty());
+
+        return new MoveData(blocksToMove, positionsToUnset);
     }
 
     public long getCoordinatesSum() {
         long sum = 0L;
         for (int y = 0; y < map.getYSize(); y++) {
             for (int x = 0; x < map.getXSize(); x++) {
-                if (map.getElementAt(x, y) == Block.BOX) {
+                if (map.getElementAt(x, y) == Block.BOX_LEFT) {
                     sum += 100 * y + x;
                 }
             }
@@ -101,17 +145,23 @@ public class Day15 {
     private static class MoveData {
 
         private final List<BlockAtPosition> blocksToMove;
+        private final List<Position> positionsToUnset;
 
         public MoveData() {
-            this(null);
+            this(null, null);
         }
 
-        public MoveData(final List<BlockAtPosition> blocksToMove) {
+        public MoveData(final List<BlockAtPosition> blocksToMove, final List<Position> positionsToUnset) {
             this.blocksToMove = blocksToMove;
+            this.positionsToUnset = positionsToUnset;
         }
 
         public List<BlockAtPosition> getBlocksToMove() {
             return blocksToMove;
+        }
+
+        public List<Position> getPositionsToUnset() {
+            return positionsToUnset;
         }
 
         public boolean canMove() {
@@ -128,12 +178,19 @@ public class Day15 {
 
     private static enum Block implements CharEnum {
 
-        PATH('.'), ROCK('#'), BOX('O'), ROBOT('@');
+        PATH('.', null), ROCK('#', null), BOX_LEFT('[', Direction.EAST), BOX_RIGHT(']', Direction.WEST),
+        ROBOT('@', null);
 
         private final char character;
+        private final Direction neighbourDirection;
 
-        private Block(final char character) {
+        private Block(final char character, final Direction neighbourDirection) {
             this.character = character;
+            this.neighbourDirection = neighbourDirection;
+        }
+
+        public Direction getNeighbourDirection() {
+            return neighbourDirection;
         }
 
         @Override
