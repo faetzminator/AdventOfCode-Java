dcb89b224ca5abcef7205b01c28ccff6574dd2ca
faetzminator <faetzminator@gmail.com>
2024-12-16 23:00:11 +0100

    initial 2023 test

---
+++ aoc2023/src/test/java/ch/faetzminator/aoc2023/Day01Test.java
@@ -0,0 +1,20 @@
+package ch.faetzminator.aoc2023;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import ch.faetzminator.aocutil.test.PuzzleTest;
+
+public class Day01Test extends PuzzleTest {
+
+    @Test
+    public void example() {
+        final String input = "1abc2\n" + "pqr3stu8vwx\n" + "a1b2c3d4e5f\n" + "treb7uchet";
+
+        final Day01 puzzle = new Day01();
+        for (final String line : toList(input)) {
+            puzzle.addLine(line);
+        }
+        Assertions.assertEquals(142L, puzzle.getCalibrationSum());
+    }
+}
---
+++ aoc2023/src/test/java/ch/faetzminator/aoc2023/Day01bTest.java
@@ -0,0 +1,21 @@
+package ch.faetzminator.aoc2023;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import ch.faetzminator.aocutil.test.PuzzleTest;
+
+public class Day01bTest extends PuzzleTest {
+
+    @Test
+    public void example() {
+        final String input = "two1nine\n" + "eightwothree\n" + "abcone2threexyz\n" + "xtwone3four\n"
+                + "4nineeightseven2\n" + "zoneight234\n" + "7pqrstsixteen";
+
+        final Day01b puzzle = new Day01b();
+        for (final String line : toList(input)) {
+            puzzle.addLine(line);
+        }
+        Assertions.assertEquals(281L, puzzle.getCalibrationSum());
+    }
+}
